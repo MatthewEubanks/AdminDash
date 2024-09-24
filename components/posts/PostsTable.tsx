80a663efe1c4bb812862fc38d8142ab97ddd995a
MatthewEubanks <spc.eubanks.matthew@gmail.com>
2024-09-24 19:28:07 +0200

    first

---
+++ components/posts/PostsTable.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+import posts from "@/data/posts";
+
+
+const PostsTable = () => {
+  return <div>posts</div>;
+};
+
+export default PostsTable;
+ 
