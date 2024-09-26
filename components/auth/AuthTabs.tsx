130c6bd3cb206e55e19df92b14627e2ea229eeee
MatthewEubanks <spc.eubanks.matthew@gmail.com>
2024-09-26 10:09:36 +0200

    a lot of changes and the app is complete

---
+++ components/auth/AuthTabs.tsx
@@ -0,0 +1,66 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import React from "react";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
+function AuthTabs() {
+  return (
+    <Tabs defaultValue="login" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="login">LOGIN</TabsTrigger>
+        <TabsTrigger value="register">REGISTER</TabsTrigger>
+      </TabsList>
+      <TabsContent value="login">
+        <Card>
+          <CardHeader>
+            <CardTitle>LOGIN</CardTitle>
+            <CardDescription>
+              Make changes to your account here. Click save when you're done.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" type="email" defaultValue="email@email.com" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="register">
+        <Card>
+          <CardHeader>
+            <CardTitle>REGISTER</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, you'll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="newUsername">Username</Label>
+              <Input id="newUsername" type="email" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="newPassword">New password</Label>
+              <Input id="newPassword" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  );
+}
+
+export default AuthTabs;
